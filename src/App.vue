e0dcc775b053607e54edf40f7aa754c89f87254f
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-02-27 23:26:21 +0900

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,17 @@
 <template>
-	<div id="app">
-		<div id="nav">
-			<router-link to="/">
-				Home
-			</router-link>
-			|
-			<router-link to="/about">
-				About
-			</router-link>
-			<TestComponent
-				:hoge="hoge"
-				fuga="string"
-			/>
-		</div>
-		<router-view />
-	</div>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <TestComponent :hoge="hoge" fuga="string" />
+    </div>
+    <router-view/>
+  </div>
 </template>
-
-<script>
-export default {
-  computed: {
-    hoge() {
-      return "hoge";
-    }
-  }
-};
-</script>
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
