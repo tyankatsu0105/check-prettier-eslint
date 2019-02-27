c7b0e03c9bd40164acdc22b15e9f4d027e105b06
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-02-27 23:00:48 +0900

    Setup vue eslint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true
+  },
+  extends: ["airbnb-base/legacy", "plugin:vue/recommended", "prettier"],
+  plugins: [prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    // Web Componentsと差別化を図るため
+    "vue/component-name-in-template-casing": "error",
+    "vue/html-closing-bracket-newline": "error",
+    "vue/html-closing-bracket-spacing": "error",
+    "vue/html-indent": ["error", "tab"],
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          normal: "never"
+        }
+      }
+    ],
+    "vue/no-multi-spaces": "error",
+    "vue/no-confusing-v-for-v-if": "error",
+    "vue/attributes-order": "error",
+    "vue/order-in-components": "error",
+    "vue/max-attributes-per-line": [
+      2,
+      {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ]
+  }
+};
